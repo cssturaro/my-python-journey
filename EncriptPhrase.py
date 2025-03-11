d0ec013a4fd83574ca9800c782df7051ed5a36d2
Cauã Sturaro <caua.sturaro111@gmail.com>
2025-03-11 16:10:40 -0300

    Create EncriptPhrase.py

---
+++ EncriptPhrase.py
@@ -0,0 +1,16 @@
+alfabet = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+            "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+            "w", "x", "y", "z"]
+
+def criptografar(frase):
+  result = ""
+  frase = str.lower(frase)
+  for i in frase:
+    if i in alfabet:
+      result = result + str((alfabet.index(i))) + " "
+    else:
+      result = result + "("+ i +")" + " "
+  return result
+
+frase = input("phrase: ")
+print("encripted phrase: " + criptografar(frase))
